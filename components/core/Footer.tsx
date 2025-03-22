ec49098564743aeeb305d962f6d99c8972dfd1d2
pyaephyowin <pyaephyowin.dev@gmail.com>
2025-03-22 09:27:22 +0630

    update footer year

--- components/core/Footer.tsx
+++ components/core/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
       <p className="text-xs text-gray-500">
-        © 2024 Memories. All rights reserved.
+        © {new Date().getFullYear()} Memories. All rights reserved.
       </p>
     </footer>
   );
